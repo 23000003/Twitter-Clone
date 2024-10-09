ccd8f822f05338efd7f75658b930657b9b34d474
23000003 <kurtdicdican@gmail.com>
2024-10-09 22:37:45 +0800

    feat: migrate to typescript

--- backend/server.ts
+++ backend/server.ts
@@ -8,7 +8,8 @@ import http from 'http';
 import SeedFakeData from './faker/seed.js';
 import { commentRouter } from './routes/CommentRoute.js';
 import { InitSocket } from './config/socketio.js';
-import SocketListener from './socket/SocketListener.js';
+import { SocketListener } from './socket/SocketListener';
+import { Response } from 'express';
 
 const app = express();
 
@@ -17,12 +18,6 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
-// Error handler
-app.use((err, req, res, next) => {
-    return res.status(500).json({
-        message: `ERROR: ${err.message}`,
-    });
-});
 
 // Routes
 app.use("/api/user", userRouter);
@@ -48,5 +43,4 @@ const server = http.createServer(app);
     } else {
         console.error('Failed to connect to MongoDB.');
     }
-    
 })();
--- backend/socket/SocketListener.ts
+++ backend/socket/SocketListener.ts
@@ -1,23 +1,24 @@
+import { Server } from "socket.io";
 
-export default function SocketListener(io){
+export function SocketListener(io : any){
 
-    io.on('connection', (socket) => {
+    io.on('connection', (socket : any) => {
         console.log('A user connected:', socket.id);
 
         //Optional as i already put in the controlelr
-        socket.on('postLiked', ({ data }) => {
-            io.emit('postLiked', { data});
+        socket.on('postLiked', ({ data } : any) => {
+            io.emit('postLiked', { data });
         });
 
-        socket.on('postUnliked', ({ data }) => {
+        socket.on('postUnliked', ({ data } : any) => {
             io.emit('postUnliked', { data });
         });
 
-        socket.on('postReposted', ({ data }) => {
+        socket.on('postReposted', ({ data } : any) => {
             io.emit('postReposted', { data });
         });
 
-        socket.on('postUndoRepost', ({ data }) => {
+        socket.on('postUndoRepost', ({ data } : any) => {
             io.emit('postUndoRepost', { data });
         });
 
